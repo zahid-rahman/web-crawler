c12ca13881ed4397bef05b6be4226be2bba9ee87
zahid-rahman <34053594+zahid-rahman@users.noreply.github.com>
2018-10-27 00:22:44 +0600

    second update with one csv file

--- rssfeed/rss.py
+++ rssfeed/rss.py
@@ -1,74 +1,69 @@
 import requests
 from bs4 import BeautifulSoup
+from bs4.builder._lxml import LXML
 
 
 # news_24 news
 
 news24_url = "http://feeds.news24.com/articles/news24/TopStories/rss"
 resp = requests.get(news24_url)
-soup = BeautifulSoup(resp.content,'lxml')
+soup = BeautifulSoup(resp.content, 'xml')
+items = soup.find_all('item')
+
+# daily star news
+daily_url = "https://www.thedailystar.net/frontpage/rss.xml"
+resp_dsn = requests.get(daily_url)
+soup_dsn = BeautifulSoup(resp_dsn.content, 'xml')
+item_dsn = soup_dsn.find_all('item')
+
 # print(soup.prettify())
 
-item = soup.find_all('item')
+
 # print(len(item))
 # print(item[0].link.text)
 
 
-fileName = "news_24.csv"
-f = open(fileName,'w')
-
-news = []
-#
-for items in item :
-    news={}
-    news['title'] = items.title.text
-    news['description'] = items.description.text
-    news['pub_date'] = items.find('pubdate').string.strip()
-    # news['link'] = items.find('link')
-    news['image'] = items.enclosure['url']
-    # print(news,"\n")
-
-    f.write("News title :"+news['title']+"\n"+"Description :"+news['description']+"\n"+"Publish date :"+news['pub_date']+"Image link :"+"\n"+news['image']+"\n\n")
-
-
-f.close()
-
-
-# daily star news
-
-daily_url = "https://www.thedailystar.net/frontpage/rss.xml"
-resp_dsn = requests.get(daily_url)
-soup_dsn = BeautifulSoup(resp_dsn.content,'lxml')
-item_dsn = soup_dsn.find_all('item')
+# fileName = "news_24.csv"
+# f = open(fileName,'w')
 
 
 
+fileName = "rssfeed.csv";
+with open(fileName, "w") as f:
 
-fileNameTwo = "daily_star_news.csv"
-f = open(fileNameTwo,'w')
+    f.write("NEWS 24"+"\n"+"link:"+news24_url+"\n\n")
 
-news_dsn = []
-#
-for items in item_dsn :
-    news_dsn={}
-    news_dsn['title'] = items.title.text
-    news_dsn['description'] = items.description.text
-    news_dsn['pub_date'] = items.find('pubdate').string.strip()
-    # news_dsn['link'] = items.find('link').text
-    # news_dsn['image'] = items.namespace('thumbnail')
-    # print(news_dsn,"\n")
+    news = []
 
-    f.write("News title :"+news_dsn['title']+"\n"+"Description :"+news_dsn['description']+"\n"+"Publish date :"+news_dsn['pub_date']+"\n\n")
+    for item in items:
+        news={}
+        news['title'] = item.title.text
+        news['description'] = item.description.text
+        news['pub_date'] = item.find('pubDate').string.strip()
+        news['link'] = item.find('link').string
+        news['image'] = item.enclosure['url']
+        # print(news,"\n")
+
+        f.write("News title :"+news['title']+"\n"+"Description :"+news['description']+"\n"+"Publish date :"+news['pub_date']+"\n"+"link: "+news['link']+"\n""Image link :"+news['image']+"\n\n")
+
+
+
+    f.write("-------------------------------------------------------------------------------------\n\n\n")
+    f.write("DAILY STAR NEWS" + "\n" + "link:" + daily_url + "\n\n")
+
+    news_dsn = []
+
+    for item in item_dsn :
+        news_dsn={}
+        news_dsn['title'] = item.title.text
+        news_dsn['description'] = item.description.text
+        news_dsn['pub_date'] = item.find('pubDate').string.strip()
+        news_dsn['link'] = item.find('link').string
+
+        # print(news_dsn,"\n")
+
+        f.write("News title :"+news_dsn['title']+"\n"+"Description :"+news_dsn['description']+"\n"+"Publish date :"+news_dsn['pub_date']+"\n"+"link: "+news_dsn['link']+"\n\n")
+        # f.write("News title :"+news_dsn['title']+"\n"+"Description :"+news_dsn['description']+"\n"+"Publish date :"+news_dsn['pub_date']+"\n\n")
 
 
 f.close()
-
-
-
-
-
-
-
-
-
-
